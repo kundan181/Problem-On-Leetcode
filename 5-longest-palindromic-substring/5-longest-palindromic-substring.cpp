b6af6a7992d941be568665fc578738f94670a09b
Kundan Shekhar Mali <93983780+kundan181@users.noreply.github.com>
2022-07-15 09:29:17 +0530

    Time: 31 ms (81.72%), Space: 7.1 MB (82.07%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
-    string longestPalindrome(string s) {
-        int n = s.size();
-        vector<vector<bool>> dp(n, vector<bool>(n, false));
-        
-        // Marking all the single letter and double letter palindromes as true
-        for(int i=0; i<n; i++) {
-            dp[i][i] = true;
-            if (i>0 && s[i-1] == s[i]) dp[i-1][i] = true;
-        }
-        
-        // Precomputing for all other possible substring
-        for(int i=n-3; i>=0; i--) {
-            for (int j=n-1; j>i+1; j--) {
-                if (s[i]==s[j] && dp[i+1][j-1]) dp[i][j] = true;  
-            }
-        }
-        
-        // Checking for the longest palindrome from all precomputed palindromes from dp matrix
-        vector<int> max_pal = {0,0};
-        int max_len = 0;
-        for(int i=0; i<n; i++) {
-            for(int j=i; j<n; j++) {
-                if(dp[i][j] && j-i+1>max_len) {
-                    max_len = j-i+1;
-                    max_pal[0] = i;
-                    max_pal[1] = j;
-                }
-            }
-        }
-        return s.substr(max_pal[0], max_len);
+   int check(string &s,int i,int j){
+    while(i>=0 && j<s.length() && s[i]==s[j]){
+        i--;
+        j++;
     }
+    return j-i-1;
+}
+//total 2*n -1 cases
+//we check fro symmetry at exh index and calculate palindrome about that point
+//then calculate maximum
+string longestPalindrome(string s) {
+    int ans=INT_MIN;
+    int idx=0;
+    for(int i=0;i<s.length();i++){
+        int ans1=max(check(s,i,i),check(s,i,i+1));
+        if(ans1>ans){
+            idx=i;
+            ans=ans1;
+        }
+       
+        
+    }
+    //diff for odd and even cases
+	//for ex ans is 9 and idx =4 we will start string from 0 i.e 4-(9-1)/2 
+	//for ex ans is 10 and idx =4 we will start string from 0 i.e 4-(10-1)/2
+	
+    return s.substr(idx-(ans-1)/2,ans);
+
+}
 };
+
