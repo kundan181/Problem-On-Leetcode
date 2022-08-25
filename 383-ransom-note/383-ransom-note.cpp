00a69acbc7dca94f839bbb1b04e7545991cb87f0
Kundan Shekhar Mali <93983780+kundan181@users.noreply.github.com>
2022-08-25 08:59:08 +0530

    Time: 29 ms (45.16%), Space: 8.7 MB (72.69%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-   bool canConstruct(string ransomNote, string magazine) {
+    bool canConstruct(string ransomNote, string magazine) {
+        //ransomeNote can be constructed from magazine if magazine contains all the letter used in making ransomeNote
+        unordered_map<char, int> m;
         
-        unordered_map<char,int> m;
-        for(int i=0;i<magazine.length();i++)
-        {
-            ++m[char(magazine[i])];
+        //store all the char of magazine so that it can be searched easily
+        for(auto x: magazine){
+            m[x]++;
         }
-        for(auto c:ransomNote)
-        {
-            if((m[c]--)==0)
+        
+		//iterate ransomeNote 
+        for(auto x: ransomNote){
+		//magazine  have this char of ransomeNote, used once, decrease frequency
+		// when hit's zero return false.(we r using post decrement)
+            if(!m[x]--){
                 return false;
+            }
         }
-        
-    
         return true;
-        
-        
-        
     }
 };
-
-
