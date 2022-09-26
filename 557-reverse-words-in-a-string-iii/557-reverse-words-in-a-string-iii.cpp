e6777fa48eada5518903cfde88245b4a4115f1d7
Kundan Shekhar Mali <93983780+kundan181@users.noreply.github.com>
2022-09-26 09:51:48 +0530

    Time: 13 ms (93.82%), Space: 9.5 MB (91.75%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -1,14 +1,31 @@
+// class Solution {
+// public:
+//     string reverseWords(string& s) {
+//         int i = 0;
+//         for (int j = 0; j < s.size(); ++j) {
+//             if (s[j] == ' ') {
+//                 reverse(s.begin() + i, s.begin() + j);
+//                 i = j + 1;
+//             }
+//         }
+//         reverse(s.begin() + i, s.end());
+//         return s;
+//     }
+// };
+
+
 class Solution {
 public:
-    string reverseWords(string& s) {
-        int i = 0;
-        for (int j = 0; j < s.size(); ++j) {
-            if (s[j] == ' ') {
+    string reverseWords(string s) {
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] != ' ') {   // when i is a non-space
+                int j = i;
+                for (; j < s.length() && s[j] != ' '; j++) { } // move j to the next space
                 reverse(s.begin() + i, s.begin() + j);
-                i = j + 1;
+                i = j - 1;
             }
         }
-        reverse(s.begin() + i, s.end());
+        
         return s;
     }
 };
