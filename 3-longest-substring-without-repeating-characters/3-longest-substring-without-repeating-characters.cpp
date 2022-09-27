3d7f18a6aa5474702b68e204ecdf6e99210107b4
Kundan Shekhar Mali <93983780+kundan181@users.noreply.github.com>
2022-09-27 08:16:35 +0530

    Time: 21 ms (69.41%), Space: 8.3 MB (60.84%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,40 +1,29 @@
-class Solution {
-public:
-     int lengthOfLongestSubstring(string s) 
-{
-    int i=0,j=0;
-    
-    unordered_map<char,int>ump;
-    int ans=0,count=0;        
-    while(j<s.length())
-    {
-        if(ump.find(s[j])==ump.end())
-        {
-            ump[s[j]]++;
-            
-            int sz=ump.size();
-            ans=max(sz,ans);
-             j++;
-            
+/*
+int lengthOfLongestSubstring(string s) {
+        vector<int> dict(256, -1);
+        int maxLen = 0, start = -1;
+        for (int i = 0; i != s.length(); i++) {
+            if (dict[s[i]] > start)
+                start = dict[s[i]];
+            dict[s[i]] = i;
+            maxLen = max(maxLen, i - start);
         }
-        else
-        {
-             int sz=ump.size();
-            ans=max(sz,ans);
-            
-            while(i<s.length() && ump.find(s[j])!=ump.end())
-            {
-                ump.erase(s[i]);
-                i++;
-            }
-            
-        }
-        
-       
+        return maxLen;
     }
-    
-    ans=max(count,ans);
-    
-    return ans;
-}
+
+*/
+class Solution{
+    public:
+    int lengthOfLongestSubstring(string s){
+        vector<int> dict(256,-1);
+        int maxLen = 0, start = -1;
+        for(int i = 0; i != s.length(); i++)
+        {
+            if(dict[s[i]] > start)
+                start = dict[s[i]];
+            dict[s[i]] = i;
+            maxLen = max(maxLen, i-start);
+        }
+        return maxLen;
+    }
 };
