0c2847fe5a39e2fc9e048683bea2cd61e6837015
Kundan Shekhar Mali <93983780+kundan181@users.noreply.github.com>
2022-09-27 08:01:45 +0530

    Time: 13 ms (41.50%), Space: 6.8 MB (67.26%) - LeetHub

--- 415-add-strings/415-add-strings.cpp
+++ 415-add-strings/415-add-strings.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     string addStrings(string num1, string num2) {
@@ -18,4 +19,21 @@ public:
         reverse(ans.begin(), ans.end());
         return ans;
     }
+};
+
+*/
+class Solution
+{
+    public:
+    string addStrings(string num1, string num2) {
+    int sum = 0, i = num1.length() - 1, j = num2.length() - 1;
+    string str;
+    while (i >= 0 || j >= 0 || sum > 0) {
+        if (i >= 0) sum += (num1[i--] - '0');
+        if (j >= 0) sum += (num2[j--] - '0');
+        str.insert(0, 1, (sum % 10) + '0');
+        sum /= 10;
+    }
+    return str;
+}
 };
