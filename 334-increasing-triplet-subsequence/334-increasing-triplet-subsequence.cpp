4b7d625315ff090bfe2acd1555ac33a6c703959c
Kundan Shekhar Mali <93983780+kundan181@users.noreply.github.com>
2022-09-28 09:23:11 +0530

    Time: 149 ms (25.30%), Space: 61.5 MB (87.71%) - LeetHub

--- 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
@@ -1,18 +1,71 @@
+/*
+    sort
+    1 2 3 4 5
+    if(i<i+1<i+2)
+
 class Solution {
 public:
-    //Trick is to find a value which a lower value on its left and a larger value towards right
-    //For this create a max array that store max value towards right till that index
-    //min till that index can be calculated simultaneously 
     bool increasingTriplet(vector<int>& nums) {
-        int mn = nums[0];
-        vector<int> mx(nums.size(), INT_MIN);
-        mx[nums.size() - 1] = nums.back();
-        for(int i = nums.size()-2; i >= 0; i--)  mx[i] = max(nums[i], mx[i+1]);
         
-        for(int i = 1; i < nums.size()-1; ++i){
-            if(nums[i] > mn && nums[i] < mx[i]) return true;
-            mn = min(mn, nums[i]);
+        for(int i=0; i<nums.size(); i++)
+        {
+            //i=0, j=i+1, k=j+1
+            //nums[i] < nums[j]
+            //nums[j] < nums[k]
+            for(int j=i+1; j<nums.size(); j++)
+            {
+                if(nums[i] < nums[j])
+                
+                for(int k=j+1; k<nums.size(); k++)
+                {
+                    if(nums[j] < nums[k])
+                    {
+                        return true;
+                    }
+                }
+            }
         }
         return false;
     }
+};
+
+
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+    
+    //sort(nums.begin(),nums.end());
+    for(int i=0; i<nums.size(); i++)
+    {
+       if(nums[i]<nums[i+1])
+       {
+           if(nums[i+1]<nums[i+2])
+           {
+               return true;
+           }
+       }
+    }
+    return false;
+    }
+};
+
+*/
+
+
+
+class Solution{
+    public:
+        bool increasingTriplet(vector<int>& nums) {
+    int c1 = INT_MAX, c2 = INT_MAX;
+    for (int x : nums) {
+        if (x <= c1) {
+            c1 = x;           // c1 is min seen so far (it's a candidate for 1st element)
+        } else if (x <= c2) { // here when x > c1, i.e. x might be either c2 or c3
+            c2 = x;           // x is better than the current c2, store it
+        } else {              // here when we have/had c1 < c2 already and x > c2
+            return true;      // the increasing subsequence of 3 elements exists
+        }
+    }
+    return false;
+}
 };
