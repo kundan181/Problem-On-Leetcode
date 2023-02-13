da3fb476bd91a9a95cde73b11c6d90c222fb977e
Kundan Shekhar Mali <93983780+kundan181@users.noreply.github.com>
2023-02-13 16:01:22 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (78.84%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -1,19 +1,10 @@
-/*
-    1.take the two pointers slow and fast 
-    2.initialise slow with head and fast with head
-    3.point slow  with head and slow with slow -> next
-    4.at last return slow
-
-*/
-class Solution{
-  public:
-    ListNode* middleNode(ListNode* head){
-        ListNode *slow = head, *fast = head;
-        while(fast && fast->next){
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        return slow;
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        auto slow = head, fast = head;
+        while(fast && fast -> next) 
+            slow = slow -> next,                                  // slow moves at 1 node / iteration
+            fast = fast -> next -> next;                          // fast moves 2 nodes / iteration
+        return slow;                                              // slow ends up at mid
     }
-    
 };
