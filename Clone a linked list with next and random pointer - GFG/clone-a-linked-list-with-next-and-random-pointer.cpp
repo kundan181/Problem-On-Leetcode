075e0305f0cd1eacbddb39925488d3adef2aa3e8
Kundan Shekhar Mali <93983780+kundan181@users.noreply.github.com>
2023-02-19 11:50:02 +0530

    Added solution - LeetHub

--- Clone a linked list with next and random pointer - GFG/clone-a-linked-list-with-next-and-random-pointer.cpp
+++ Clone a linked list with next and random pointer - GFG/clone-a-linked-list-with-next-and-random-pointer.cpp
@@ -28,50 +28,72 @@ class Solution
         {
             head = newNode;
             tail = newNode;
-            return;
         }
         else
         {
             tail->next = newNode;
-            tail = newNode;   
+            tail = newNode;
         }
     }
     public:
     Node *copyList(Node *head)
     {
         //Write your code here
+        //step1: create a clone list
         Node* cloneHead = NULL;
         Node* cloneTail = NULL;
         
         Node* temp = head;
-        //step1 clone the ll
         while(temp != NULL)
         {
             insertAtTail(cloneHead, cloneTail, temp->data);
             temp = temp->next;
         }
         
-        //step2 : create a map
-        unordered_map<Node*, Node*> oldToNewNode;
+        
+        //step2: cloneNodes add in between original List
         Node* originalNode = head;
-        Node* cloneNode = cloneHead;
-        while(originalNode != NULL)
+        Node* cloneNode =  cloneHead;
+        
+        while(originalNode != NULL && cloneNode != NULL)
         {
-            oldToNewNode[originalNode] = cloneNode;
-            originalNode = originalNode->next;
-            cloneNode = cloneNode->next;
+            Node* next = originalNode->next;
+            originalNode->next = cloneNode;
+            originalNode = next;
+            next = cloneNode->next;
+            cloneNode->next = originalNode;
+            cloneNode = next;
         }
+        //step3: random pointer copy 
+        temp = head;
+        while(temp != NULL)
+        {
+            if(temp->next != NULL){
+                temp->next->arb = temp->arb 
+                ? temp->arb->next : temp->arb; 
+            }
+            temp = temp->next->next;
+        }
+        
+        //step4: revert changes done in step2
         
         originalNode = head;
         cloneNode = cloneHead;
-        
-        while(originalNode != NULL)
+        while(originalNode != NULL && cloneNode != NULL)
         {
-            cloneNode->arb = oldToNewNode[originalNode->arb];
+            originalNode->next = cloneNode->next;
             originalNode = originalNode->next;
+            
+             if(originalNode != NULL)
+            {
+                cloneNode->next = originalNode->next;
+            }
             cloneNode = cloneNode->next;
+           
         }
+        //step5: return ans.
         return cloneHead;
+        
     }
 
 };
