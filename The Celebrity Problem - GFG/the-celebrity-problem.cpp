49d3e93911e488d04a394aca63ebb32e6e790d8a
Kundan Shekhar Mali <93983780+kundan181@users.noreply.github.com>
2023-02-22 16:40:31 +0530

    Added solution - LeetHub

--- The Celebrity Problem - GFG/the-celebrity-problem.cpp
+++ The Celebrity Problem - GFG/the-celebrity-problem.cpp
@@ -48,44 +48,34 @@ class Solution
                 s.push(a);
             }
         }
-        int candidate = s.top();
+        int ans = s.top();
         //step3 : single element in stack is potential celeb
         //so verify it
-        bool rowCheck = false;
         int zeroCount = 0;
         for(int i=0; i<n; i++)
         {
-            if(M[candidate][i] == 0){
+            if(M[ans][i] == 0){
                 zeroCount++;
             }
         }
         //all zeroes
-        if(zeroCount == n)
-        {
-            rowCheck = true;
-        }
+        if(zeroCount != n)
+            return -1;
         // column check 
-        bool colCheck = false;
         int oneCount = 0;
         
         for(int i=0; i<n; i++)
         {
-            if(M[i][candidate] == 1)
+            if(M[i][ans] == 1)
             {
                 oneCount++;
             }
         }
-        if(oneCount == n-1){
-            colCheck = true;
+        if(oneCount != n-1){
+                return -1;
         }
         
-        if(rowCheck == true && colCheck == true)
-        {
-            return candidate;
-        }
-        else{
-            return -1;
-        }
+        return ans;
     }
 };
 
