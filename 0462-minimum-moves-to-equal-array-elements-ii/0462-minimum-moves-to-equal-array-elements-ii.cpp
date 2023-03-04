2dd610f53c63cd6b4596e1f1e2cfc7b5d4fd54e8
Kundan Shekhar Mali <93983780+kundan181@users.noreply.github.com>
2023-03-04 14:25:06 +0530

    Time: 7 ms (98.30%), Space: 11.9 MB (69.19%) - LeetHub

--- 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -1,23 +1,53 @@
+
+//Approch 1 : finding the median and compare it with mean 
 //NlogN -> Time
 //O(1)  -> Space
+// class Solution {
+// public:
+//     int minMoves2(vector<int>& nums) {
+        
+//         int n = nums.size();
+//         //sort the array
+//         sort(nums.begin(), nums.end());
+//         //find the median
+//         int median = n%2==0 ? (nums[n/2] + nums[n/2-1]) / 2 : nums[n/2];
+        
+//         int steps = 0;
+        
+//         for(int num : nums)
+//         {
+//             steps += abs(num-median);
+//         }
+//         return steps;
+//     }
+// };
 
+
+
+
+//Approch 2 : two pointer tech
+//NlogN -> Time
+//O(1)  -> Space
 class Solution {
 public:
     int minMoves2(vector<int>& nums) {
         
-        int n = nums.size();
-        //sort the array
-        sort(nums.begin(), nums.end());
-        //find the median
-        int median = n%2==0 ? (nums[n/2] + nums[n/2-1]) / 2 : nums[n/2];
+        //sort the array first
+        sort(nums.begin(),nums.end());
+        
+        //calculate the steps by taking two pointers
         
         int steps = 0;
+        int start = 0;
+        int end = nums.size()-1;
         
-        for(int num : nums)
+        while(start <= end)
         {
-            steps += abs(num-median);
+            steps += nums[end] - nums[start];
+            start++;
+            end--;
         }
-        return steps;
         
+        return steps;
     }
 };
